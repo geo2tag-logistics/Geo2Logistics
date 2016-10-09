99c64082e473ea99d2092d956d726202671c9881
Mtdr <paul.osmolovsky@gmail.com>
2016-10-10 01:10:00 +0300

    0.0.1b(1)
    +profiles

--- logistics/views.py
+++ logistics/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     # return render(request, 'logistics/login.html'),
-    return render(request, 'logistics/driver-fleets.html')
+    return render(request, 'logistics/driver-profile.html')
 
 def addFleet(request):
     return render(request, 'logistics/addFleet.html')
