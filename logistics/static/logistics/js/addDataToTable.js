f1c3969d6864aff7e1a77797de018471fca5da6d
Mtdr <paul.osmolovsky@gmail.com>
2016-10-27 01:21:07 +0300

    0.0.2b(2)
    + angular.js + bower

---
+++ logistics/static/logistics/js/addDataToTable.js
@@ -0,0 +1,26 @@
+var addDataApp=angular.module('addDataApp');
+addDataApp.controller('dataController', function($scope) {
+
+    $scope.taxiServices = [
+        {
+            name: 'Yandex',
+            description: "very good work at Russia",
+            creation_date: "2016-10-22T14:01:23Z",
+            cars_count:1,
+            trips_count:1
+        },
+        {
+            name: 'Uber',
+            description: "good at EU",
+            creation_date: "2016-10-22T14:01:23Z",
+            cars_count:21,
+            trips_count:31
+        },
+        {
+            name: 'Get',
+            description: "rather good",
+            creation_date: "2016-10-22T14:01:23Z",
+            cars_count:11,
+            trips_count:13
+        }]
+});
